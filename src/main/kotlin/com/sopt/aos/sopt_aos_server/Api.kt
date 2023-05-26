14a24f3c6c549ce5ce412a0c0bfabc27952b989b
머환임 <udsward@gmail.com>
2023-05-27 00:59:47 +0900

    upload multipart

--- src/main/kotlin/com/sopt/aos/sopt_aos_server/Api.kt
+++ src/main/kotlin/com/sopt/aos/sopt_aos_server/Api.kt
@@ -1,18 +1,51 @@
 package com.sopt.aos.sopt_aos_server
 
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.ObjectMetadata
 import com.fasterxml.jackson.annotation.JsonInclude
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MaxUploadSizeExceededException
+import org.springframework.web.multipart.MultipartFile
+import java.util.*
 
 @RestController
 class Api(
-    private val logic: Logic
+    private val logic: Logic,
+    @Qualifier("s3C") private val s3Client: AmazonS3Client
 ) {
 
+    @PostMapping("/upload")
+    fun upload(
+        file: MultipartFile
+    ): ResponseEntity<Response<UploadResponse>>? {
+        val uuid = UUID.randomUUID().toString()
+        try {
+            s3Client.putObject(
+                "my-daehwan-bucket",
+                uuid,
+                file.inputStream,
+                ObjectMetadata().apply {
+                    contentType = file.contentType;
+                    contentLength = file.size
+                })
+        } catch (t: Throwable) {
+            throw UploadEx()
+        }
+        return ResponseEntity.ok(
+            Response.success(
+                "업로드 성공",
+                UploadResponse(imageUrl = "https://my-daehwan-bucket.s3.ap-northeast-2.amazonaws.com/${uuid}")
+            )
+        );
+    }
+
     @GetMapping("/readiness")
-    fun healthCheck() : Response<Nothing?> {
+    fun healthCheck(): Response<Nothing?> {
         return Response.success(message = "서버 통신에 성공했습니다.", data = null)
     }
 
@@ -84,8 +117,17 @@ class Api(
         ResponseEntity.status(400).body(Response.error(message = e.message ?: "잘못된 접급입니다."))
 
     @ExceptionHandler(DataIntegrityViolationException::class)
-    fun handleDataException(e: DataIntegrityViolationException) : ResponseEntity<Response<Nothing>> =
+    fun handleDataException(e: DataIntegrityViolationException): ResponseEntity<Response<Nothing>> =
         ResponseEntity.status(409).body(Response.error(message = "중복된 아이디로 가입을 시도했습니다."))
+
+    @ExceptionHandler(UploadEx::class)
+    fun handleUpload(e: UploadEx): ResponseEntity<Response<Nothing>> =
+        ResponseEntity.status(500).body(Response.error(message = "이미지 업로드 중 오류가 발셍했습니다."))
+
+
+    class UploadEx : RuntimeException() {
+
+    }
 }
 
 
@@ -130,3 +172,15 @@ data class SignInRequest(
     val id: String,
     val password: String
 )
+
+data class UploadResponse(
+    val imageUrl: String
+)
+
+@ControllerAdvice
+class advice {
+    @ExceptionHandler(MaxUploadSizeExceededException::class)
+    fun handleUploadOver(e: MaxUploadSizeExceededException): ResponseEntity<Response<Nothing>> =
+        ResponseEntity.status(400).body(Response.error(message = "100 KB 이하의 이미지를 사용하세요."))
+
+}
--- src/main/kotlin/com/sopt/aos/sopt_aos_server/Configuration.kt
+++ src/main/kotlin/com/sopt/aos/sopt_aos_server/Configuration.kt
@@ -1,7 +1,14 @@
 package com.sopt.aos.sopt_aos_server
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseFactory
@@ -15,7 +22,10 @@ import javax.sql.DataSource
         DataSourceAutoConfiguration::class
     ]
 )
-class Configuration {
+@ConfigurationPropertiesScan
+class Configuration(
+    private val awsProperties: AwsProperties
+) {
 
     @Bean
     fun datasource(): DataSource {
@@ -25,4 +35,21 @@ class Configuration {
 
         return embeddedDataSourceFactory.database
     }
+
+    @Bean("s3C")
+    fun s3(): AmazonS3Client {
+        val basicAWSCredentials = BasicAWSCredentials(awsProperties.accessKey, awsProperties.secretKey)
+
+        return AmazonS3ClientBuilder.standard()
+            .withRegion("ap-northeast-2")
+            .withCredentials(AWSStaticCredentialsProvider(basicAWSCredentials))
+            .build() as AmazonS3Client
+    }
+
+    @ConstructorBinding
+    @ConfigurationProperties(prefix = "cloud.aws.credentials")
+    data class AwsProperties(
+        val accessKey: String,
+        val secretKey: String
+    )
 }
